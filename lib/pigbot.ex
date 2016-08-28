78a82e3744ced62fff1c8648be828a3f275575d8
Ally Weir <ally.pcgf@gmail.com>
2016-08-28 13:51:48 +0100

    fix thing

--- lib/pigbot.ex
+++ lib/pigbot.ex
@@ -4,7 +4,7 @@ defmodule Pigbot.Router do
   get "/" do
     conn = put_resp_content_type(conn, "application/json")
 
-    message = %{"response_type" => "in_channel", "text", "Pizza Pizza :pizza: :pizza:"}
+    message = %{"response_type" => "in_channel", "text" => "Pizza Pizza :pizza: :pizza:"}
     |> Poison.encode!([])
 
     send_resp(conn, 200, message)
